fe6deb07e166e091e9f41bcda1afba7b51000995
kitchottrakarnW <kitchottrakarn_w@silpakorn.edu>
2019-05-21 15:41:37 +0700

    User & Password (Input)

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,6 +6,20 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showPassword() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'Password :', hintText: 'More than 6 charactor'),
+    );
+  }
+
+  Widget showUser() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'User :', hintText: 'your username...'),
+    );
+  }
+
   Widget showLogo() {
     return Image.asset('images/logo.png');
   }
@@ -17,8 +31,6 @@ class _AuthenState extends State<Authen> {
           fontSize: 30.0, fontWeight: FontWeight.bold, color: Colors.red[900]),
     );
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +44,14 @@ class _AuthenState extends State<Authen> {
           Container(
             margin: EdgeInsets.only(top: 10.0),
             child: showTitle(),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50.0, right: 50.0),
+            child: showUser(),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 50.0, right: 50.0),
+            child: showPassword(),
           )
         ],
       ),
